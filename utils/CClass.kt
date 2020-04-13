bb9771d72fe7f00d4d14543a05a9ab6ba2e3ad1c
Artyom Mishin <themrlokopoff@gmail.com>
2020-04-13 13:56:13 +0300

    Update

--- utils/CClass.kt
+++ utils/CClass.kt
@@ -1,7 +1,13 @@
 package engine.utils
 
 import engine.CEngine
+import engine.utils.module.CModule
 
 abstract class CClass {
-    protected val eng: CEngine = engine.eng;
+    protected val eng: CEngine
+            get() = engine.eng;
+
+    inline fun <reified module: CModule> getModule() : module {
+        return engine.eng.getModule();
+    }
 }
